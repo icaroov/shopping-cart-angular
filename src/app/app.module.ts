f597761562ac5c6ee2506fe17325a5471dab5345
Icaro Oliveira <icarovinici@gmail.com>
2021-01-15 15:53:53 -0300

    refactor: edit somes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
-import { NavComponent } from './components/template/nav/nav.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent,
     FooterComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    CartComponent,
+    ProductListComponent,
+    FiltersComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
