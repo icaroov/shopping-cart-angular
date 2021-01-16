ef8051c927d0f2ca86427ace18317873521c081a
Icaro Oliveira <icarovinici@gmail.com>
2021-01-16 17:51:56 -0300

    refactor: improves cart and cart-item component

--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MessengerService } from "src/app/services/messenger.service";
+
+import { CartService } from "src/app/services/cart.service";
+import { CartItem } from "src/app/models/cart-item";
+import { Product } from "src/app/models/product";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"],
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  cartItems: any = [];
+
+  cartTotal = 0;
+
+  constructor(
+    private msg: MessengerService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
+    this.handleGetProduct();
+    this.loadCartItems();
   }
 
+  handleGetProduct() {
+    this.msg.getMsg().subscribe(() => {
+      this.loadCartItems();
+    });
+  }
+
+  loadCartItems() {
+    this.cartService.getCartItems().subscribe((items: CartItem[]) => {
+      this.cartItems = items;
+      this.getTotal();
+    });
+  } 
+
+  getTotal() {
+    this.cartTotal = 0;
+    this.cartItems.forEach((item: CartItem) => {
+      this.cartTotal += (item.qty * item.price);
+    });
+  }
 }
